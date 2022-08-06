4666241c077d41433b43d62fb1c17380a12d0b02
Marcos Andrade <marcos_q_andrade@hotmail.com>
2022-08-06 00:39:13 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/dscatalog/DSCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSCatalogApplication.class, args);
+	}
+
+}
