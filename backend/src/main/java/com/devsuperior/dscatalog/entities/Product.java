01587be6064589f2cc8fc79f04a863f1e574e0b5
Marcos Andrade <marcos_q_andrade@hotmail.com>
2022-08-08 14:09:59 -0300

    Product entity, sql script

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dscatalog.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Product implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant date;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id")
+            ,inverseJoinColumns = @JoinColumn(name = "category_id"))
+    @ToString.Exclude
+    private Set<Category> categories = new HashSet<>();
+
+
+}
